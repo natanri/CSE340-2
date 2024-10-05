b3c5fe5d88b659db5473a20a0f484c2191a96026
natanri <riosjhonatan44@gmail.com>
2024-10-05 10:56:46 -0500

    config port and host

--- server.js
+++ server.js
@@ -75,12 +75,12 @@ app.use(async(err, req, res, next) =>{
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 10000
+const host = process.env.HOST || '0.0.0.0'
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on Host: ${host}, Port :${port}`)
 })
