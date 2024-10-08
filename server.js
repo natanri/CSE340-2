79853e29f61d22b585de41268f581c8293aa76eb
natanri <riosjhonatan44@gmail.com>
2024-10-08 11:23:01 -0500

    force to listen on 0.0.0.0

--- server.js
+++ server.js
@@ -73,11 +73,11 @@ app.use(async(err, req, res, next) =>{
  * Values from .env (environment) file
  *************************/
 const port = process.env.PORT || 3000
-const host = process.env.HOST || '0.0.0.0'
+const host = '0.0.0.0'
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`app listening on ${host}:${port}`)
 })
